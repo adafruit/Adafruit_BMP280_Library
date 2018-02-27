ea1cbc8d219ea9170f953aa76fbd8e573447af26
Martin <floggy22@googlemail.com>
2018-02-27 21:45:38 +0100

    add  base frame

--- Adafruit_BMP280.h
+++ Adafruit_BMP280.h
@@ -123,6 +123,42 @@ class Adafruit_BMP280_Unified : public Adafruit_Sensor
 class Adafruit_BMP280
 {
   public:
+  /*
+     enum sensor_sampling {
+            SAMPLING_NONE = 0b000,
+            SAMPLING_X1   = 0b001,
+            SAMPLING_X2   = 0b010,
+            SAMPLING_X4   = 0b011,
+            SAMPLING_X8   = 0b100,
+            SAMPLING_X16  = 0b101
+        };
+
+        enum sensor_mode {
+            MODE_SLEEP  = 0b00,
+            MODE_FORCED = 0b01,
+            MODE_NORMAL = 0b11
+        };
+
+        enum sensor_filter {
+            FILTER_OFF = 0b000,
+            FILTER_X2  = 0b001,
+            FILTER_X4  = 0b010,
+            FILTER_X8  = 0b011,
+            FILTER_X16 = 0b100
+        };
+
+        // standby durations in ms 
+        enum standby_duration {
+            STANDBY_MS_0_5  = 0b000,
+            STANDBY_MS_10   = 0b110,
+            STANDBY_MS_20   = 0b111,
+            STANDBY_MS_62_5 = 0b001,
+            STANDBY_MS_125  = 0b010,
+            STANDBY_MS_250  = 0b011,
+            STANDBY_MS_500  = 0b100,
+            STANDBY_MS_1000 = 0b101
+        };
+    */
     Adafruit_BMP280();
     Adafruit_BMP280(int8_t cspin);
     Adafruit_BMP280(int8_t cspin, int8_t mosipin, int8_t misopin, int8_t sckpin);
