bf6af0fa991171d3668eb8aab4e175235e3652ca
Travis CI <travis@travis-ci.org>
2019-02-28 17:17:04 +0000

    Deploy code docs to GitHub Pages Travis build: 5
    Commit: 3805bc5e192b3b2262de9f7882ef8fab421e0b73

--- html/search/searchdata.js
+++ html/search/searchdata.js
@@ -5,8 +5,7 @@ var indexSectionsWithContent =
   2: "abrs",
   3: "d",
   4: "s",
-  5: "fms",
-  6: "a"
+  5: "fms"
 };
 
 var indexSectionNames =
@@ -16,8 +15,7 @@ var indexSectionNames =
   2: "functions",
   3: "variables",
   4: "enums",
-  5: "enumvalues",
-  6: "pages"
+  5: "enumvalues"
 };
 
 var indexSectionLabels =
@@ -27,7 +25,6 @@ var indexSectionLabels =
   2: "Functions",
   3: "Variables",
   4: "Enumerations",
-  5: "Enumerator",
-  6: "Pages"
+  5: "Enumerator"
 };
 
