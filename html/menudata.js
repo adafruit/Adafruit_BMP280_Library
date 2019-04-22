98d9619d5bb0733c363547bedd3fe56f959cb039
Travis CI <travis@travis-ci.org>
2019-04-22 10:50:44 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: 104df207134a7819d7966e3d52e17a44358ca317

--- html/menudata.js
+++ html/menudata.js
@@ -5,6 +5,7 @@ var menudata={children:[
 {text:"Class Index",url:"classes.html"},
 {text:"Class Members",url:"functions.html",children:[
 {text:"All",url:"functions.html",children:[
+{text:"_",url:"functions.html#index__"},
 {text:"a",url:"functions.html#index_a"},
 {text:"b",url:"functions.html#index_b"},
 {text:"d",url:"functions.html#index_d"},
@@ -17,4 +18,8 @@ var menudata={children:[
 {text:"Enumerations",url:"functions_enum.html"},
 {text:"Enumerator",url:"functions_eval.html"}]}]},
 {text:"Files",url:"files.html",children:[
-{text:"File List",url:"files.html"}]}]}
+{text:"File List",url:"files.html"},
+{text:"File Members",url:"globals.html",children:[
+{text:"All",url:"globals.html"},
+{text:"Enumerator",url:"globals_eval.html"},
+{text:"Macros",url:"globals_defs.html"}]}]}]}
