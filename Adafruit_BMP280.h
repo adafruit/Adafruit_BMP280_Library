1d1dca7c0ba39b668adf2930cca88e39eeb247cf
Steven Slupsky <sslupsky@gmail.com>
2019-05-10 12:17:20 -0600

    forward declaration required when Wire or SPI interface count = 0

--- Adafruit_BMP280.h
+++ Adafruit_BMP280.h
@@ -33,6 +33,10 @@
   (0x76)                     /**< Alternative I2C address for the sensor. */
 #define BMP280_CHIPID (0x58) /**< Default chip ID. */
 
+//  Forward declarations of Wire and SPI for board/variant combinations that don't have a default 'Wire' or 'SPI' 
+extern TwoWire Wire;
+extern SPIClass SPI;
+
 /*!
  * Registers available on the sensor.
  */
