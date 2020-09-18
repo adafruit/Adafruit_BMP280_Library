ea973517d5d57837475412e31404e57a75a836cb
jschwender <joachim.schwender@web.de>
2020-09-18 14:45:38 +0200

    add a function

--- Adafruit_BMP280.h
+++ Adafruit_BMP280.h
@@ -195,7 +195,7 @@ public:
   float readPressure(void);
   float readAltitude(float seaLevelhPa = 1013.25);
   float seaLevelForAltitude(float altitude, float atmospheric);
-  float WaterBoilingPoint(float Pressure);
+  float waterBoilingPoint(float Pressure);
 
   Adafruit_Sensor *getTemperatureSensor(void);
   Adafruit_Sensor *getPressureSensor(void);
