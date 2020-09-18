71eaa33577e8e21050a94b81d94877cee592a847
jschwender <joachim.schwender@web.de>
2020-09-18 16:10:45 +0200

    Update Adafruit_BMP280.cpp

--- Adafruit_BMP280.cpp
+++ Adafruit_BMP280.cpp
@@ -401,8 +401,8 @@ float Adafruit_BMP280::seaLevelForAltitude(float altitude, float atmospheric) {
 float Adafruit_BMP280::waterBoilingPoint(float Pressure) {   
   // Magnusformular for calculation of the boiling point of water at a given
   // pressure
-  return (234.175 * log(Pressure/6.1078)) / 
-	 (17.08085 - log(Pressure/6.1078) );
+  return (234.175 * log(Pressure/6.1078)) /
+	 (17.08085 - log(Pressure/6.1078));
 }
 
 /*!
